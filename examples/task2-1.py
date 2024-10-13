b91ada3e3f0b960d95197e7a8ee87351ab4affdd
digitaldnq <denqcr19@gmail.com>
2024-10-13 17:50:43 +0500

    Create task2-1.py
    Определение и вызов функции

---
+++ examples/task2-1.py
@@ -0,0 +1,13 @@
+def example1():
+
+    print("Вывод первого example: Hello, World! (c) Daniel Mukhamedov")
+
+example1()
+
+def example2():
+    return True
+
+example = example2()
+print("Вывод второго example: ")
+print(type(example2()))
+print(example2())
